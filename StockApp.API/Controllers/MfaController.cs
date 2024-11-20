e46b7c8513e6ee3e959b61259a6fbe047ff06313
Icaro Bellem <icarobellemskr@hotmail.com>
2024-11-20 16:44:22 -0300

    Tarefa 29 - Implementar Autenticação Multi-Fator (MFA)

---
+++ StockApp.API/Controllers/MfaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MfaController : ControllerBase
+{
+    private readonly IMfaService _mfaService;
+
+    public MfaController(IMfaService mfaService)
+    {
+        _mfaService = mfaService;
+    }
+
+    [HttpPost("generate")]
+    public IActionResult GenerateOtp([FromBody] string userId)
+    {
+        var otp = _mfaService.GenerateOtp(userId);
+        return Ok(new { Otp = otp });
+    }
+
+    [HttpPost("validate")]
+    public IActionResult ValidateOtp([FromBody] ValidateOtpRequest request)
+    {
+        var isValid = _mfaService.ValidateOtp(request.UserId, request.UserOtp);
+        return Ok(new { IsValid = isValid });
+    }
+}
+
+public class ValidateOtpRequest
+{
+    public string UserId { get; set; }
+    public string UserOtp { get; set; }
+}
