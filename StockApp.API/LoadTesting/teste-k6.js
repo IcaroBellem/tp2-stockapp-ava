5e59a0f287a4d14654d5b5831aa10e8aa02db39b
Icaro Bellem <icarobellemskr@hotmail.com>
2024-11-21 07:50:03 -0300

    Tarefa 53 - Implementar Testes de Carga (Load Testing)

---
+++ StockApp.API/LoadTesting/teste-k6.js
@@ -0,0 +1,21 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    vus: 50,
+    duration: '30s',
+};
+
+export default function () {
+
+    let url = 'https://localhost:7189/api/Categories';
+
+    let res = http.get(url);
+
+    check(res, {
+        'status é 200': (r) => r.status === 200,
+        'respondeu em menos de 500ms': (r) => r.timings.duration < 500,
+    });
+
+    sleep(1); 
+}
